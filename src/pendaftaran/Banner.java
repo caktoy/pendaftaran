bbd79fd9bb12fb874ddb063788e500febc329940
Thony Hermawan <thony.caktoy@gmail.com>
2015-02-16 21:41:28 +0700

    First commit

---
+++ src/pendaftaran/Banner.java
@@ -0,0 +1,29 @@
+package pendaftaran;
+
+/**
+ *
+ * @author Thony
+ */
+
+import java.awt.Graphics;  
+import java.awt.Graphics2D;  
+import java.awt.Image;  
+import javax.swing.ImageIcon;  
+import javax.swing.JPanel;
+
+public class Banner extends JPanel{
+    private Image image;  
+  
+    public Banner() {  
+        image = new ImageIcon(getClass().getResource("banner.jpg")).getImage();  
+    }  
+  
+    @Override
+    protected void paintComponent(Graphics grphcs) {  
+        super.paintComponent(grphcs);  
+  
+        Graphics2D gd = (Graphics2D) grphcs.create();  
+        gd.drawImage(image, 0, 0, getWidth(), getHeight(), null);  
+        gd.dispose();  
+    }
+}
---
+++ src/pendaftaran/Pendaftaran.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pendaftaran;
+
+/**
+ *
+ * @author Thony
+ */
+public class Pendaftaran {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FormMain form = new FormMain();
+        form.setVisible(true);
+        form.show();
+    }
+    
+}
